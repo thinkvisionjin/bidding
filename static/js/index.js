ec4bd3aac0f1764f72f510ce9fd98a95e0bf1fc4
JinLiu <thinkvisionjin@163.com>
2016-04-24 15:29:25 +0800

    add datbase

---
+++ static/js/index.js
@@ -0,0 +1,22 @@
+$(document).ready(function () {
+//            $('#mainSplitter').jqxSplitter({ width: '100%', height: window.innerHeight*0.95, panels: [{ size: 200 }] });
+			$('#mainSplitter').jqxSplitter({ width: '100%', height: '100%', panels: [{ size: 200 }] });
+			$("#jqxNavigationBar").jqxNavigationBar({height: "100%", width: "100%", expandMode: "singleFitHeight"});
+			$('#jqxTabs').jqxTabs({height: '100%', showCloseButtons: true, scrollPosition: 'both' });
+			$("#yh").jqxButton({ theme: theme });
+			$("#yh1").jqxButton({ theme: theme });
+			$('#jqxTree').jqxTree({ height: '300px', width: '300px'});
+        });
+		function addTabs(frameid, url)
+		{
+			var length = $('#jqxTabs').jqxTabs('length'); 
+			for (var i = 0;i<length ;i++ )
+			{
+				 if ($('#jqxTabs').jqxTabs('getTitleAt', i) == frameid)
+				 {
+					 $('#jqxTabs').jqxTabs('select', i);
+					 return ;
+				 }
+			}
+			$('#jqxTabs').jqxTabs('addAt', 1, frameid, '<iframe id="'+frameid+'" style="border:none;padding:0;margin:0;" frameborder=no border=0 height="99%" width="100%"src="'+url+'"></iframe>' );
+		}
