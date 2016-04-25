0996333114e90786436233b8048b142c6c053a7e
square <square@192.168.1.100>
2016-04-25 21:55:47 +0800

    a

--- static/js/vendor/jqwidgets/demos.js
+++ static/js/vendor/jqwidgets/demos.js
@@ -7,7 +7,7 @@ function getDemoTheme() {
         return theme;
     }
 
-    var theme = "office";
+    var theme = "metro";
    
 
     var url = "/bidding/static/css/vendor/jqwidgets/styles/jqx." + theme + '.css';
