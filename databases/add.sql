d47dd470ab6ce8a6ebee5e1b89efabfcc0c6be01
square <square@zxm>
2016-06-09 09:03:13 +0800

    update background

--- databases/add.sql
+++ databases/add.sql
@@ -5,3 +5,6 @@ insert into hys(hys) values('第二会议室')
 insert into hys(hys) values('第三会议室')
 insert into hys(hys) values('第四会议室')
 insert into hys(hys) values('多媒体会议室')
+
+	SELECT *  FROM (select employeeid, purchaseStyleId from [BIDDING].[dbo].[Project] )a
+  pivot (count(employeeid) for PurchaseStyleId in ([1], [2], [3], [4],[5], [6],[7]))b
