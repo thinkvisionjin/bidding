34155775cf125d0170884f7930c6474cf99e1c69
square <square@192.168.1.100>
2016-08-17 16:28:42 +0800

    aa

---
+++ static/js/gmbs1.js
@@ -0,0 +1,230 @@
+
+function search()
+{
+	if ($("#dwmc").val()=='' && $("#bsbh").val()=='')
+		{
+		url = "select_gmbs";
+		}
+	else
+		{
+		url = "select_gmbs?dwmc="+$("#dwmc").val()+"&bsbh="+$("#bsbh").val()
+		}
+		
+	var source = {					
+		datatype : "json",
+		datafields : [{name : 'Id',type : 'string'	},
+{name : 'dwmc',type : 'string'	},
+{name : 'rq',type : 'date'	},
+{name : 'zzszwmc',type : 'string'	},
+{name : 'zzsywmc',type : 'string'	},
+{name : 'zzsgb',type : 'string'	},
+{name : 'lxdz',type : 'string'	},
+{name : 'lxr',type : 'string'	},
+{name : 'sj',type : 'string'	},
+{name : 'dzxx',type : 'string'	},
+{name : 'cz',type : 'string'	},
+{name : 'je',type : 'string'	},
+{name : 'bsbh',type : 'string'	},
+{name : 'username',type : 'string'	},
+{name : 'ly',type : 'string'	}],
+		id : 'Id',
+		url : url
+	};
+	var dataAdapter = new $.jqx.dataAdapter(source);
+	$("#gmbs-grid").jqxGrid({
+		source : dataAdapter
+	});	
+}
+
+function addselectfieldwindows()
+{
+	$(document.body).append('<div id="gmbszd_popupWindow" ><div>字段选择</div><div style="overflow: hidden;"><div  id="gmbs_zdlistbox"></div></div></div>');
+	$("#gmbszd_popupWindow").jqxWindow({ isModal: true, autoOpen: false, height: 300, width: 200 , modalOpacity: 0.5});
+	 var listSource = [{ label: '序号', value: 'Id', checked: true },,
+{ label: '购标书单位名称', value: 'dwmc', checked: true },,
+{ label: '日期', value: 'rq', checked: true },,
+{ label: '制造商单位中文名称', value: 'zzszwmc', checked: true },,
+{ label: '制造商单位英文名称', value: 'zzsywmc', checked: false },,
+{ label: '制造商国别', value: 'zzsgb', checked: false },,
+{ label: '联系地址', value: 'lxdz', checked: false },,
+{ label: '联系人', value: 'lxr', checked: true },,
+{ label: '手机', value: 'sj', checked: true },,
+{ label: '电子信箱', value: 'dzxx', checked: false },,
+{ label: '传真', value: 'cz', checked: false },,
+{ label: '标书编号', value: 'bsbh', checked: true },,
+{ label: '金额', value: 'je', checked: true },,
+{ label: '操作人', value: 'username', checked: false },,
+{ label: '来源', value: 'ly', checked: false },];
+	$('#gmbs_zdlistbox').jqxListBox({ source: listSource, width:'100%', height:'100%', checkboxes: true });
+    $("#gmbs_zdlistbox").on('checkChange', function (event) {
+        $("#gmbs-grid").jqxGrid('beginupdate');
+        if (event.args.checked) {
+            $("#gmbs-grid").jqxGrid('showcolumn', event.args.value);
+        }
+        else {
+            $("#gmbs-grid").jqxGrid('hidecolumn', event.args.value);
+        }
+        $("#gmbs-grid").jqxGrid('endupdate');
+    });	
+}
+
+
+function modifygmbs(id)
+{
+	gmbs_popupwindow('modify', id, search);
+
+}
+
+function deletegmbs(id)
+{
+	if (confirm('是否删除')==false)
+	{
+		return ;
+	}
+    var selectedrowindex = $("#gmbs-grid").jqxGrid('getselectedrowindex');
+    var rowscount = $("#gmbs-grid").jqxGrid('getdatainformation').rowscount;
+    if (selectedrowindex >= 0 && selectedrowindex < rowscount) {
+        var rowid = $("#gmbs-grid").jqxGrid('getrowid', selectedrowindex);
+        
+    }
+
+	$.get('deleterow_gmbs?Id='+id, function(result){
+		if (result == 'success')
+		{
+			confirm('成功')
+			$("#gmbs-grid").jqxGrid('deleterow', rowid);
+		}
+		else
+		{
+			alert(result)
+		}	
+	});
+}
+
+function printgmbs(id)
+{
+//	window.location.href='gmbs_print?Id='+id;
+
+    var newWindow = window.open('gmbs_print?Id='+id, '');
+
+    newWindow.print();	
+//	window.location.replace ('gmbsmx?oper=modify&Id='+id);
+}
+
+function detailgmbs(id)
+{
+	gmbs_popupwindow('detail', id);
+//	window.location.replace ('gmbsmx?oper=modify&Id='+id);
+}
+function configpopupwindow()
+{
+
+	gmbs_init();
+}
+
+$(document).ready(function() {
+	
+					$("#gmbs-expander").jqxExpander({
+						toggleMode : 'none',
+						showArrow : false,
+						height: '100%'
+					});
+					$("#gmbs-grid")
+							.jqxGrid(
+									{
+										enabletooltips: true,
+										columnsresize: true,
+										height : "90%",
+										width : "98%",
+										columns : [{ text: '序号', datafield: 'Id', width: '10%',cellsalign: 'center', align: 'center',hidden:false },
+{ text: '购标书单位名称', datafield: 'dwmc', width: '10%',cellsalign: 'center', align: 'center',hidden:false },
+{ text: '日期', datafield: 'rq', cellsformat:'yyyy-MM-dd HH:mm:ss', width: '10%',cellsalign: 'center', align: 'center',hidden:false },
+{ text: '制造商单位中文名称', datafield: 'zzszwmc', width: '10%',cellsalign: 'center', align: 'center',hidden:false },
+{ text: '制造商单位英文名称', datafield: 'zzsywmc', width: '10%',cellsalign: 'center', align: 'center',hidden:true },
+{ text: '制造商国别', datafield: 'zzsgb', width: '10%',cellsalign: 'center', align: 'center',hidden:true },
+{ text: '联系地址', datafield: 'lxdz', width: '10%',cellsalign: 'center', align: 'center',hidden:true },
+{ text: '联系人', datafield: 'lxr', width: '10%',cellsalign: 'center', align: 'center',hidden:false },
+{ text: '手机', datafield: 'sj', width: '10%',cellsalign: 'center', align: 'center',hidden:false },
+{ text: '电子信箱', datafield: 'dzxx', width: '10%',cellsalign: 'center', align: 'center',hidden:true },
+{ text: '传真', datafield: 'cz', width: '10%',cellsalign: 'center', align: 'center',hidden:true },
+{ text: '标书编号', datafield: 'bsbh', width: '10%',cellsalign: 'center', align: 'center',hidden:false },
+{ text: '金额', datafield: 'je', width: '10%',cellsalign: 'center', align: 'center',hidden:false },
+{ text: '操作人', datafield: 'username', width: '10%',cellsalign: 'center', align: 'center',hidden:true },
+{ text: '来源', datafield: 'ly', width: '10%',cellsalign: 'center', align: 'center',hidden:true },
+												{
+													text : '操作',
+													width: '200',
+													editable : false,
+													datafield : 'delete',
+													cellsrenderer : function(
+															index, datafield,
+															value,
+															defaultvalue,
+															column, rowdata) {
+														var a = '<a style="margin-right: 5px;padding-top:3px;height:15px;text-decoration:none;" class="MdyBtn" onclick="printgmbs('+rowdata.Id+')">打印</a>';
+														if(rowdata['ly']=='交易流水确认')
+															{
+															b = '';
+															c = '';
+											
+															}
+														else
+															{
+																b = '<a style="margin-right: 5px;padding-top:3px;height:15px;text-decoration:none;" class="MdyBtn" onclick="modifygmbs('+rowdata.Id+')">修改</a>';
+
+																c = '<a style="margin-right: 5px;;padding-top:3px;height:15px;text-decoration:none;" class="MdyBtn" onclick="deletegmbs('+rowdata.Id+')">删除</a>';
+															}
+														var d = '<a style="margin-right: 5px;;padding-top:3px;height:15px;text-decoration:none;" class="MdyBtn" onclick="detailgmbs('+rowdata.Id+')">详细</a>';
+														var d = '<div class="jqx-grid-cell-middle-align" style="margin-top: 6px;">'
+																+ a+b+c+d + '</div>';
+														return d;
+													}
+												} ],
+										showtoolbar : true,
+										rendertoolbar : function(toolbar) {
+											var me = this;
+											var container = $("<div style='margin: 5px;'></div>");
+											toolbar.append(container);
+											container.append('<input id="gmbsadd" type="button" value="新增" />');
+											$("#gmbsadd").jqxButton({
+												template : 'success'
+											});
+											/*container.append('<input id="gmbsexport" type="button" value="输出" />');
+											$("#gmbsexport").jqxButton({
+												template : 'primary'
+											});	*/										
+											container.append('<input id="selectfiled" style="float: right" type="button" value="设置" />');
+											$("#selectfiled").jqxButton({
+												template : 'info'
+											});											
+										}
+
+									});
+					//$("#gmbs-grid").('hidecolumn', 'id');
+					search();
+					/*$("#gmbsexport").click(function() {
+						$("#gmbs-grid").jqxGrid('exportdata', 'xls', 'jqxGrid');           
+						//window.location.replace('gmbsmx');
+						//$("#popupWindow").jqxWindow('open');
+					});*/
+					$("#gmbsadd").click(function() {
+						gmbs_popupwindow('add', '', search);
+						//window.location.replace('gmbsmx');
+						//$("#popupWindow").jqxWindow('open');
+					});
+					$("#selectfiled").click(function() {
+						$("#gmbszd_popupWindow").jqxWindow('open');
+					});					
+					$("#search").jqxButton({
+						template : 'primary'
+					});	
+					$("#search").click(function() {
+						search();
+					});					
+
+					$('#dwmc').jqxInput();
+					$('#bsbh').jqxInput();
+					addselectfieldwindows();
+					configpopupwindow();
+
+				});
---
+++ static/js/xmzcmxform.js
@@ -0,0 +1,241 @@
+
+function getkh(dwmc)
+{
+	if (dwmc == olddwmc)
+		{
+		return;
+		}
+	$.get('getkh?dwmc='+dwmc, function(result){
+		if (result.length==1) {
+			var data = result[0];
+			$('#xmzc_lxdz').val(data['lxdz']);
+			$('#xmzc_dzxx').val(data['dzxx']);
+			$('#xmzc_cz').val(data['cz']);
+		}
+		else
+		{
+			$('#xmzc_lxdz').val('');
+			$('#xmzc_dzxx').val('');
+			$('#xmzc_cz').val('');
+		}
+		olddwmc = dwmc;
+		
+	}, 'json');	
+}
+function xmzc_configpage(projectid)
+{
+	if (projectid == undefined)
+	{
+		url = 'getxmzcpz';
+	}
+	else
+	{
+		url = 'getxmzcpz?projectid='+projectid;
+	}
+	
+	$.get(url, function(result){
+		//需特殊处理
+    	$('#xmzc_projectid').jqxDropDownList({ placeHolder: "", source: result['projectid'], displayMember:'ProjectCode', valueMember:'Id'});
+$('#xmzc_sz').jqxDropDownList({ placeHolder: "", source: result['sz']});
+$('#xmzc_projectid').jqxDropDownList('selectIndex', 0);
+$('#xmzc_ywlx').jqxDropDownList({ placeHolder: "", source: result['ywlx']});
+    }, 'json');	
+    $("#xmzc_dwmc input").blur(function(){getkh($("#xmzc_dwmc").val())});  
+}
+var olddwmc='';
+var state = 'add';
+function xmzc_setupadd()
+{
+	$('#xmzc_Id').val('');
+$('#xmzc_projectid').jqxDropDownList('selectIndex',-1);
+$('#xmzc_sz').jqxDropDownList('selectIndex',-1);
+$('#xmzc_zy').val('');
+$('#xmzc_je').val('');
+$('#xmzc_ywlx').jqxDropDownList('selectIndex',-1);
+$('#xmzc_lyId').val('');
+$('#xmzc_username').val('');
+$('#xmzc_rq').val('');;
+	$('#tr_xmzc_lyId').hide();
+$('#tr_xmzc_username').hide();
+$('#tr_xmzc_rq').hide();;
+	$('#xmzc_Save').show();
+	$('#xmzc_Cancel').val('取消');
+}
+function xmzc_setupdetail()
+{
+	$('#tr_xmzc_lyId').show();
+$('#xmzc_lyId').jqxInput({disabled:true});
+$('#tr_xmzc_username').show();
+$('#xmzc_username').jqxInput({disabled:true});
+$('#tr_xmzc_rq').show();
+$('#xmzc_rq').jqxInput({disabled:true});
+	$.get('selectone_xmzc?Id='+$('#xmzc_Id').val(), function(result){
+		var data = result[0];
+		$('#xmzc_Id').val(data['Id']);
+$('#xmzc_projectid').val(data['projectid']);
+$('#xmzc_sz').val(data['sz']);
+$('#xmzc_zy').val(data['zy']);
+$('#xmzc_je').val(data['je']);
+$('#xmzc_ywlx').val(data['ywlx']);
+$('#xmzc_lyId').val(data['lyId']);
+$('#xmzc_username').val(data['username']);
+$('#xmzc_rq').val(data['rq']);		
+	}, 'json');	
+	$('#xmzc_Save').hide();
+	$('#xmzc_Cancel').val('关闭');
+	
+}
+function xmzc_setupmodify()
+{
+	$('#tr_xmzc_lyId').show();
+$('#xmzc_lyId').jqxInput({disabled:true});
+$('#tr_xmzc_username').show();
+$('#xmzc_username').jqxInput({disabled:true});
+$('#tr_xmzc_rq').show();
+$('#xmzc_rq').jqxInput({disabled:true});;
+	$.get('selectone_xmzc?Id='+$('#xmzc_Id').val(), function(result){
+		var data = result[0];
+		$('#xmzc_Id').val(data['Id']);
+$('#xmzc_projectid').val(data['projectid']);
+$('#xmzc_sz').val(data['sz']);
+$('#xmzc_zy').val(data['zy']);
+$('#xmzc_je').val(data['je']);
+$('#xmzc_ywlx').val(data['ywlx']);
+$('#xmzc_lyId').val(data['lyId']);
+$('#xmzc_username').val(data['username']);
+$('#xmzc_rq').val(data['rq']);			
+	}, 'json');	
+	$('#xmzc_Save').show();
+	$('#xmzc_Cancel').val('取消');	
+}
+
+var gkhcallback;
+function xmzc_save()
+{
+	if (state == 'add')
+		{
+		url = 'insertrow_xmzc';
+		}
+	else if (state == 'modify')
+		{
+		url = 'updaterow_xmzc?Id='+$('#xmzc_Id').val();
+		}
+	var row = {	
+	projectid:$('#xmzc_projectid').val(),
+sz:$('#xmzc_sz').val(),
+zy:$('#xmzc_zy').val(),
+je:$('#xmzc_je').val(),
+ywlx:$('#xmzc_ywlx').val(),
+lyId:$('#xmzc_lyId').val(),
+username:$('#xmzc_username').val(),
+	//////////来源////需特殊处理//////
+	};
+    $.ajax({
+        url:url,
+        type:'POST',
+        data:row,
+        success:function(response,status,xhr){
+            
+            if (response == 'success')
+            	{
+				alert('成功');
+            	$('#xmzc_popupWindow').jqxWindow('hide');
+				gkhcallback();
+            	}
+			else
+			{
+				alert(response);
+			}
+             
+        },
+        error:function(xhr,errorText,errorType){
+            alert("操作不成功");
+        },
+    })	
+}
+
+function xmzc_init () {
+	$(document.body).append("       <div id='xmzc_popupWindow'>\
+            <div>项目支出</div>\
+            <div style='overflow: hidden;'><div id='xmzcmx-expander'>\
+		<div id='xmzc_title'>新增</div>\
+		<div>\
+		\
+			<table align='center' >\
+			<tr id='tr_xmzc_Id' style='display:none'><td class='tbinputtitle'>序号:</td><td><input class='tbinput' type='text' id='xmzc_Id'/></td></tr>\
+<tr id='tr_xmzc_projectid'><td class='tbinputtitle'>项目编号:</td><td><div class='tbinput' type='text' id='xmzc_projectid'/></td></tr>\
+<tr id='tr_xmzc_sz'><td class='tbinputtitle'>收支:</td><td><div class='tbinput' type='text' id='xmzc_sz'/></td></tr>\
+<tr id='tr_xmzc_zy'><td class='tbinputtitle'>摘要:</td><td><input class='tbinput' type='text' id='xmzc_zy'/></td></tr>\
+<tr id='tr_xmzc_je'><td class='tbinputtitle'>金额:</td><td><input class='tbinput' type='text' id='xmzc_je'/></td></tr>\
+<tr id='tr_xmzc_ywlx'><td class='tbinputtitle'>业务类型:</td><td><div class='tbinput' type='text' id='xmzc_ywlx'/></td></tr>\
+<tr id='tr_xmzc_lyId'><td class='tbinputtitle'>来源序号:</td><td><input class='tbinput' type='text' id='xmzc_lyId'/></td></tr>\
+<tr id='tr_xmzc_username'><td class='tbinputtitle'>操作人:</td><td><input class='tbinput' type='text' id='xmzc_username'/></td></tr>\
+<tr id='tr_xmzc_rq'><td class='tbinputtitle'>日期:</td><td><input class='tbinput' type='text' id='xmzc_rq'/></td></tr>\
+\
+\
+                    <tr>\
+                        <td align='right'></td>\
+                        <td style='padding-top: 10px;' align='right'><input style='margin-right: 5px;' type='button' id='xmzc_Save' value='保存' /><input id='xmzc_Cancel' type='button' value='取消' /></td>\
+                    </tr>\
+                </table>\
+		</div>");
+	$("#xmzc_popupWindow").jqxWindow({
+		width: 600, height:360, resizable: true,  isModal: true, autoOpen: false, cancelButton: $("#xmzc_Cancel"), modalOpacity: 0.4           
+	});	
+	$("#xmzcmx-expander").jqxExpander({ toggleMode: 'none',  showArrow: false });
+
+    // Create a jqxInput
+    
+
+    $('#xmzc_Id').jqxInput();
+$('#xmzc_projectid').jqxDropDownList({ placeHolder: ''});
+$('#xmzc_sz').jqxDropDownList({ placeHolder: ''});
+$('#xmzc_zy').jqxInput();
+$('#xmzc_je').jqxNumberInput({inputMode: 'simple'});
+$('#xmzc_ywlx').jqxDropDownList({ placeHolder: ''});
+$('#xmzc_lyId').jqxInput();
+$('#xmzc_username').jqxInput();
+$('#xmzc_rq').jqxInput();
+	$('#tr_xmzc_lyId').hide();
+$('#tr_xmzc_username').hide();
+$('#tr_xmzc_rq').hide();
+
+	
+    $("#xmzc_Save").jqxButton({template:'success'});
+    $("#xmzc_Cancel").jqxButton({template:'warning'});    
+
+	$("#xmzc_Save").click(function () {
+		xmzc_save(state);
+	});
+ 	
+  	
+}
+
+function xmzc_popupwindow(flag_state, id, callback, projectid)
+{
+	y = document.body.scrollTop;
+	x = (document.body.scrollWidth -600)/2
+	$('#xmzc_popupWindow').jqxWindow({ position: { x: x, y: y }});		
+	state = flag_state;
+	gkhcallback = callback;
+	$('#xmzc_Id').val(id);
+	$('#xmzc_projectid').val(projectid);
+	xmzc_configpage(projectid);
+	if (state == 'add')
+	{
+		xmzc_title.innerHTML='新增';
+		xmzc_setupadd();
+	}
+	if (state == 'modify')
+	{
+		xmzc_title.innerHTML='修改';
+		xmzc_setupmodify();
+	}
+	if (state == 'detail')
+		{
+			
+			xmzc_title.innerHTML='详情';
+			xmzc_setupdetail();
+		}
+	$('#xmzc_popupWindow').jqxWindow('open');
+}
